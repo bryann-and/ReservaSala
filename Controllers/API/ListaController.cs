e449202cb6694badd545eebf18161a2fbf4eedb9
Bryan Anderson Duarte <bryann.and@gmail.com>
2019-08-03 19:08:54 -0300

    inicial

---
+++ Controllers/API/ListaController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ReservaSala.Models;
+
+namespace ReservaSala.Controllers.API
+{
+    [Route("api/Lista")]
+    [ApiController]
+    public class ListaSalaAPI : ControllerBase
+    {
+        [HttpGet]
+        public List<Sala> Get()
+        {
+            return new List<Sala>
+            {
+                new Sala() { Nome = "Sala 01" },
+                new Sala() { Nome = "Sala 02" }
+            };
+        }
+
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+    }
+}
---
+++ Controllers/SalasController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ReservaSala.Controllers.API;
+using ReservaSala.Models;
+
+namespace ReservaSala.Controllers
+{
+    public class SalasController : Controller
+    {
+        // GET: Salas
+        public ActionResult Index()
+        {
+            ListaSalaAPI lsita = new ListaSalaAPI();
+
+            return View("ListaSalas", lsita.Get());
+        }
+
+        // GET: Salas/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Salas/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Salas/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Salas/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
